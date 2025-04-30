ddfb2e58594d378ce4dec3f4358c569908f5f8f2
Hakushii0 <kiryuuin3@gmail.com>
2025-04-30 14:56:13 +0000

    Wed, Apr 30, 2025, 4:56 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,8 +2,8 @@ file_name = input("File name: ").lower()
 suffix = file_name.split(".")[1]
 
 dict = {
-    "gif": "
-    "jpg":
+    "gif": "image/gif",
+    "jpg": "
     "jpeg":
     "png":
     "pdf":
