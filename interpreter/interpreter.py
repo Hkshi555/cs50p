c35cbc357196e01c18c7661eb66f2b5ac71100c3
Hakushii0 <kiryuuin3@gmail.com>
2025-04-30 15:22:02 +0000

    Wed, Apr 30, 2025, 5:22 PM +02:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,19 +1,14 @@
-expression = input("Enter expression: ")
+expression = input("Enter expression: ").strip()
 split_expression = expression.split(" ")
+
 x = int(split_expression[0])
 y = split_expression[1]
 z = int(split_expression[2])
-ans = 0
 
-match y:
-    case "+":
-        ans = x + y
-    case "-":
-        ans = x - y
-    case "/":
-        ans = x / y
-    case "*":
-        ans = x * y
+ans = eval(f"{x} {y} {z}")
+
+
+
 
 print(float(ans))
 
