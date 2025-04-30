879a19a725ef8043239fb088b604abfd026acd37
Hakushii0 <kiryuuin3@gmail.com>
2025-04-30 14:59:14 +0000

    Wed, Apr 30, 2025, 4:59 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,11 +3,16 @@ suffix = file_name.split(".")[1]
 
 dict = {
     "gif": "image/gif",
-    "jpg": "
-    "jpeg":
-    "png":
-    "pdf":
-    "txt":
-    "zip":
+    "jpg": "image/jpg",
+    "jpeg": "image/jpeg",
+    "png": "image/png",
+    "pdf": "application/pdf",
+    "txt": "text/plain",
+    "zip": "application/zip",
 }
+
+if not dict.get(suffix):
+    print("application/octet-stream")
+else:
+    print(dict[suffix])
 
