138099f73064da26ba16d10c5c85dd3ca0e16dc4
Hakushii0 <kiryuuin3@gmail.com>
2025-04-30 15:24:04 +0000

    Wed, Apr 30, 2025, 5:24 PM +02:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -5,9 +5,19 @@ x = int(split_expression[0])
 y = split_expression[1]
 z = int(split_expression[2])
 
-ans = eval(f"{x} {y} {z}")
 
+# Can be cheated with this
+#ans = eval(f"{x} {y} {z}")
 
+match y:
+    case "+":
+        ans = x + z
+    case "-":
+        ans = x - z
+    case "/":
+        ans = x / z
+    case "*":
+        ans = x * z
 
 
 print(float(ans))
---
+++ meal/meal.py
@@ -0,0 +1,10 @@
+def main():
+    time = input("Enter time: ").strip()
+
+
+def convert(time):
+    
+
+
+if __name__ == "__main__":
+    main()
