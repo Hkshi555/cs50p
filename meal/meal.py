64e009af9450c70d11af3c2f3598e51b22d5d59e
Hakushii0 <kiryuuin3@gmail.com>
2025-04-30 15:31:08 +0000

    Wed, Apr 30, 2025, 5:31 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,19 @@
 def main():
     time = input("Enter time: ").strip()
-    
+
+    converted = convert(time)
+    print(converted)
+
+    if converted :
+        print("breakfast time")
+    elif converted in range(12, 13):
+        print("lunch time")
+    elif converted in range(18, 19):
+        print("dinner time")
 
 def convert(time):
-    hour = time.split(":")[0]
-    minutes = time.split(":")[1]
+    hour = int(time.split(":")[0])
+    minutes = int(time.split(":")[1])
 
 
     return float(hour + (minutes / 60))
