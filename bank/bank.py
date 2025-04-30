a33abea00f85b1152f976a67d64a8d7839558cb4
Hakushii0 <kiryuuin3@gmail.com>
2025-04-30 14:49:05 +0000

    Wed, Apr 30, 2025, 4:49 PM +02:00

---
+++ bank/bank.py
@@ -0,0 +1,10 @@
+greeting = input("Greeting: ")
+
+if greeting.startswith("hello"):
+    print("$0")
+
+elif greeting.startswith("h") and not greeting.startswith("hello"):
+    print("$20")
+
+else:
+    print("$200")
