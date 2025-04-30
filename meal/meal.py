16704438bb9e4cee29c213eb38350f619999463c
Hakushii0 <kiryuuin3@gmail.com>
2025-04-30 15:37:08 +0000

    Wed, Apr 30, 2025, 5:37 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -3,11 +3,11 @@ def main():
 
     converted = convert(time)
 
-    if converted >= 7.0 and converted =< 8.0:
+    if converted >= 7.0 and converted <= 8.0:
         print("breakfast time")
-    elif converted >= 12.0 and converted =< 13.0:
+    elif converted >= 12.0 and converted <= 13.0:
         print("lunch time")
-    elif converted >= 18.0 and converted =< 19.0:
+    elif converted >= 18.0 and converted <= 19.0:
         print("dinner time")
 
 def convert(time):
